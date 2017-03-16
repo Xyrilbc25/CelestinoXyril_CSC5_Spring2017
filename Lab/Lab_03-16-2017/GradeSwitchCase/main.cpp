5aa36d481dc53d289d900e642e922295d2b097ec
rcc <rcc>
2017-03-16 12:27:42 -0700

    Signed-off-by: rcc <rcc>

---
+++ Lab/Lab_03-16-2017/GradeSwitchCase/main.cpp
@@ -0,0 +1,50 @@
+/* 
+ * File:   GradeDependentIf
+ * Author: Xyril Celestino
+ * Created on March 16, 2017, 11:24 AM
+ * Purpose:  Program that calculates grades
+ */
+
+//System Libraries
+#include <iostream>  //Input - Output Library
+using namespace std; //Name-space under which system libraries exist
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here
+int main(int argc, char** argv) {
+    //Declare and Initialize variables
+    unsigned short score;   //Integer scores valid from 0 to 100
+    char grade;
+    
+    //Input data
+    cout<<"This program produces a grade from a score input"<<endl;
+    cout<<"The data type used is an integer [0-100]"<<endl;
+    cout<<"Type in the score"<<endl;
+    cin>>score;
+    if (!(score>=0&&score<=100)){
+        cout<<"You failed to input a score between 0 and 100"<<endl;
+        return 1;       //Use DeMorgans law to make clearer
+    }   
+    
+    //Map inputs to outputs or process the data
+    switch  (score/10){
+        case  10:
+        case   9: grade = 'A';break;
+        case   8: grade = 'B';break;
+        case   7: grade = 'C';break;
+        case   6: grade = 'D';break;
+        default : grade = 'F';
+    }
+    
+    //Output the transformed data
+    cout<<"For a score = "<<score<<" your grade is a(n) "<<grade<<"."<<endl;
+    
+    //Exit stage right!
+    return 0;
+}
+
