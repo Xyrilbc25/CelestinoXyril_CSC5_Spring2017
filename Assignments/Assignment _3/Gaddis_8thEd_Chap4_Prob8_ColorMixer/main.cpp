4cc0ef0f7a6ffd2f2a1c37251214860ec8460c8a
Xyril Celestino <xyrilbc@yahoo.com>
2017-03-20 09:55:16 -0700

    Signed-off-by: Xyril Celestino <xyrilbc@yahoo.com>

--- Assignments/Assignment _3/Gaddis_8thEd_Chap4_Prob8_ColorMixer/main.cpp
+++ Assignments/Assignment _3/Gaddis_8thEd_Chap4_Prob8_ColorMixer/main.cpp
@@ -13,22 +13,27 @@ using namespace std; //Name-space under which system libraries exist
 //User Libraries
 
 //Global Constants
+const int SECONDARYCOLOR = prmcol1+prmcol2;     //Secondary = two primary colors
 
 //Function Prototypes
 
 //Execution begins here
 int main(int argc, char** argv) {
     //Declare and initialize variables
-    
+    int prmcol1, prmcol2;         //Two Primary Colors
+    char ;                //Secondary Color
+   
     //Input data
     cout<<"This program tells the secondary color of two primary ";
     cout<<"colors that are mixed (red,blue,yellow)"<<endl;
-    cout<<"please input two colors"<<endl;
-    cin>>
+    cout<<"please input two colors (red,yellow,green)"<<endl;
+    cin>>color1>>color2;
     
     //Map inputs to outputs or process the data
     
     //Output the transformed data
+    cout<<"For two primary colors, "<<color1<<" "<<color2<<"the secondary ";
+    cout<<"color would be = "<<secCol<<endl;
     
     //Exit stage right!
     return 0;
