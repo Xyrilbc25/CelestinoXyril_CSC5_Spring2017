d088a6ecbf1ad49cf2ff0b4c3f092771c9c54944
Xyril Celestino <xyrilbc@yahoo.com>
2017-03-21 10:56:29 -0700

    Signed-off-by: Xyril Celestino <xyrilbc@yahoo.com>

--- Assignments/Assignment _3/Gaddis_8thEd_Chap4_Prob8_ColorMixer/main.cpp
+++ Assignments/Assignment _3/Gaddis_8thEd_Chap4_Prob8_ColorMixer/main.cpp
@@ -8,32 +8,47 @@
 
 //System Libraries
 #include <iostream>  //Input - Output Library
+#include <string>    //string library
+#include <cctype>    //toupper, tolower
 using namespace std; //Name-space under which system libraries exist
 
 //User Libraries
 
 //Global Constants
-const int SECONDARYCOLOR = prmcol1+prmcol2;     //Secondary = two primary colors
 
 //Function Prototypes
 
 //Execution begins here
 int main(int argc, char** argv) {
     //Declare and initialize variables
-    int prmcol1, prmcol2;         //Two Primary Colors
-    char ;                //Secondary Color
+    string col1, col2;         //The 2 Primary colors inputted
+    
+    //Input variables
+    cout<<"This program combines 2 primary colors into a secondary color"<<endl;
+    cout<<"Please input 2 primary colors (Red, Blue, Yellow)"<<endl;
+    cin>>col1>>col2;
+    //Test for correctness
+    if (!(col1 == "Red"||col1 == "Blue"||col1 == "Yellow")){
+        cout<<"Sorry, input primary colors only (red, blue, yellow)"<<endl;
+        return 1;   //Exit Program
+    }
+    if (!(col2 == "Red"||col2 == "Blue"||col2 == "Yellow")){
+        cout<<"Sorry, input primary colors only (Red, Blue, Yellow)"<<endl;
+        return 1;   //Exit Program
+    }
+    if (col1 == col2){
+        cout<<"Same colors, input two different primaries"<<endl;
+    }
    
-    //Input data
-    cout<<"This program tells the secondary color of two primary ";
-    cout<<"colors that are mixed (red,blue,yellow)"<<endl;
-    cout<<"please input two colors (red,yellow,green)"<<endl;
-    cin>>color1>>color2;
-    
     //Map inputs to outputs or process the data
+    cout<<col1<<" and "<<col2<<" result in the following"<<endl;
+    if ((col1 =="Red"&& col2 =="Blue")||(col2 =="Red"&& col1 =="Blue"))
+        cout<<"The mixture of those two colors is Purple"<<endl;
+    if ((col1 =="Red"&& col2 =="Yellow")||(col2 =="Red"&& col1 =="Yellow"))
+        cout<<"The mixture of those two colors is Orange"<<endl;
+    if ((col1 =="Yellow"&& col2 =="Blue")||(col2 =="Yellow"&& col1 =="Blue"))
+        cout<<"The mixture of those two colors is Green"<<endl;
     
-    //Output the transformed data
-    cout<<"For two primary colors, "<<color1<<" "<<color2<<"the secondary ";
-    cout<<"color would be = "<<secCol<<endl;
     
     //Exit stage right!
     return 0;
