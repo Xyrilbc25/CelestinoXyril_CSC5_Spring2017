d153d19c5d460b818a6b38252955900f174950a4
Xyril Celestino <xyrilbc@yahoo.com>
2017-03-28 15:31:54 -0700

    Signed-off-by: Xyril Celestino <xyrilbc@yahoo.com>

---
+++ Assignments/Assignment #4/Gaddis_8thEd_Chap5_Prob9_ChangeForDollar/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   Gaddis_8thEd_Chap5_Prob9_ChangeforDollar
+ * Author: Xyril Celestino
+ * Created on March 28, 2017, 3:30 PM
+ * Purpose:  Program that converts change into dollars
+ */
+
+//System Libraries
+#include <iostream>  //Input - Output Library
+#include <cmath>     //Math Library
+
+using namespace std; //Name-space under which system libraries exist
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here
+int main(int argc, char** argv) {
+    //Declare and initialize variables
+    
+    //Input data
+    
+    //Map inputs to outputs or process the data
+    
+    //Output the transformed data
+    
+    //Exit stage right!
+    return 0;
+}
+
