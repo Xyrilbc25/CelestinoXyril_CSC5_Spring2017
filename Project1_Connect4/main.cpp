f00d65b23ac1659029e60984e875cba612387bbc
Xyril Celestino <xyrilbc@yahoo.com>
2017-04-17 11:22:56 -0700

    Signed-off-by: Xyril Celestino <xyrilbc@yahoo.com>

--- Project1_Connect4/main.cpp
+++ Project1_Connect4/main.cpp
@@ -15,9 +15,9 @@ using namespace std; //Name-space under which system libraries exist
 
 //Function Prototypes
 void display();
-bool check(int a, int b);
-int  drop(int b,char player);
-char place[6][7];
+bool check(int a, int b);           //Check if game is won
+int  drop(int b,char player);       //Drop pieces
+char place[6][7];                   //6x7 grid
 
 //Execution begins here
 int main(int argc, char** argv) {
@@ -26,20 +26,20 @@ int main(int argc, char** argv) {
             place[a][b] = ' ';
     }
     display();                      //Displays board
-    int hold;                       //User row choice
-    int hold2    = 0;               //hold drop values
-    int PcPlace  = 0;               //Number of pieces dropped
-    bool gamewin = false;
+    int  hold;                      //User row choice
+    int  hold2   = 0;               //Hold drop values
+    int  PcPlace = 0;               //Number of pieces dropped
+    bool gamewin = false;           //Checks if game is won
     char player  = 15;              //Switches between player 1 and 2
     while(!gamewin){                //Stops when game is won
         if(hold2 != -1){
             if(player == 15){
                 cout<<"Player 1 choose where to drop piece";
-                player = 254;       //Char of player's piece
+                player = 254;       //Char of player 1's piece
             }
             else{
                 cout<<"Player 2 choose where to drop piece";
-                player = 15;        //Char of players piece
+                player = 15;        //Char of player 2's piece
             }
         }
         while(true){
@@ -48,62 +48,67 @@ int main(int argc, char** argv) {
             hold--;
             if(hold <=6 && hold>= 0) break;
             else cout<<"please enter a value between 1 and 7 = ";
-            if (cin.fail())
+            if (cin.fail())         //Makes sure only works if 1-7 is inputted
             {
                 cin.clear();
                 char c;
-                cin>>c;
+                cin>>c;             //Input where player chooses where piece lands
             }
         }
         if (PcPlace == 42) break;
         hold2 = drop (hold, player);
-        if (hold2 == -1) cout<<"Column is full,",
+        if (hold2 == -1) cout<<"Column is full,",   //Checks if column is full
                 "please input number between 1 and 7 = ";
         else{
             gamewin = check(hold2,hold);
             PcPlace ++;
-            system("cls");          //Clears screen
+            cout<<endl<<endl<<endl;
             display();              //displays updated board
         }
     }
-    system("cls");
-    if (PcPlace == 42){             //If Draw between bothe player
+    cout<<endl<<endl<<endl;
+    if (PcPlace == 42){             //If Draw between both players
         cout<<"Draw! No one wins!"<<endl;
-        system("pause");
         return 0;
     }
-    if(player == 15)                //If won by player 2
-        cout<<"The Winner is...Player 2!"<<endl;
+    if(player == 15)                              //If won by player 2
+         cout<<"The Winner is...Player 2!"<<endl;
     else cout<<"The Winner is...Player 1!"<<endl; //If won by player 1
-    system("pause");                //pauses so players can see who won
+    
     return 0;                       //Exit Application
 }
 void display(){
-    cout<<"1    2   3   4   5   6   7"<<endl;
+    cout<<"         Connect 4!      "<<endl;
+    cout<<"        Player 1 = []    "<<endl;
+    cout<<"        Player 2 = |     "<<endl;
+    cout<<"1   2   3   4   5   6   7"<<endl;
     for(int a = 0; a<= 5; a++)
     {
-        for(int b=0; b <=6; b++)
-            cout<<char(218)<<char(196)<<char(191)<<" ";
+        for(int b=0; b <=6; b++){
+            cout<<"----";           //Horizontal line separation on board
+        }
         cout<<endl;
-        for(int b=0; b <=6; b++)
-            cout<<char(179)<<place[a][b]<<char(179)<<" ";
+        for(int b=0; b <=6; b++){
+            cout<<"|"<<place[a][b]<<"|"<<" ";
+        //cout<<endl;               //Vertical line separation on board
+        }
         cout<<endl;
-        for(int b=0; b <=6; b++)
-            cout<<char(192)<<char(196)<<char(217)<<" ";
-        cout<<endl;
+        for(int b=0; b <=6; b++){
+            //cout<<"_";
+        }
     }
 }
 bool check(int a, int b){
-    int vertical   = 1; //(|)
-    int horizontal = 1; //(-)
-    int diagonal1  = 1; //(\)
-    int diagonal2  = 1; //(/)
-    char player    = place[a][b];
-    int i;              //Vertical
-    int ii;             //Horizontal
+    int vertical   = 1; //Vertical win
+    int horizontal = 1; //Horizontal win
+    int diagonal1  = 1; //Diagonal win (\)
+    int diagonal2  = 1; //Diagonal win (/)
+    char player    = place[a][b];   //Placement of pieces
+    int i;              //Vertical placement
+    int ii;             //Horizontal placement
     //Check for Verticals
-    for(i=a +1;place[i][b] == player && i <=5;i++,vertical++);
-    for(i=a +1;place[i][b] == player && i >=0;i--,vertical++);
+    for(i=a -1;place[i][b] == player && i >=0;i--,vertical++);
+    for(i=a +1;place[i][b] == player && i <=6;i++,vertical++);
     if (vertical >= 4) return true;
     //Check for Horizontals
     for(ii=b -1;place[a][ii] == player && ii >=0;ii--,horizontal++);
@@ -141,4 +146,5 @@ int drop(int b, char player){
     }
     
 }
+//Exit Program
 
