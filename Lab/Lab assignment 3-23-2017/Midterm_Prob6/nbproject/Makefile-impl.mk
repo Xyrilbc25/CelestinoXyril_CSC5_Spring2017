fb8ab04e31b12de43713ae07a4ac7c3b7f7ee7f0
Xyril Celestino <xyrilbc@yahoo.com>
2017-04-25 11:24:08 -0700

    Signed-off-by: Xyril Celestino <xyrilbc@yahoo.com>

--- Lab/Lab assignment 3-23-2017/Midterm_Prob6/nbproject/Makefile-impl.mk
+++ Lab/Lab assignment 3-23-2017/Midterm_Prob6/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=Midterm_Prob2
+PROJECTNAME=Midterm_Prob6
 
 # Active Configuration
 DEFAULTCONF=Debug
--- Lab/Lab assignment 3-23-2017/Midterm_Prob6/nbproject/Makefile-variables.mk
+++ Lab/Lab assignment 3-23-2017/Midterm_Prob6/nbproject/Makefile-variables.mk
@@ -9,19 +9,19 @@ CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=Cygwin-Windows
 CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
-CND_ARTIFACT_NAME_Debug=midterm_prob2
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/midterm_prob2
+CND_ARTIFACT_NAME_Debug=midterm_prob6
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/midterm_prob6
 CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
-CND_PACKAGE_NAME_Debug=midtermprob2.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/midtermprob2.tar
+CND_PACKAGE_NAME_Debug=midtermprob6.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/midtermprob6.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
-CND_ARTIFACT_NAME_Release=midterm_prob2
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/midterm_prob2
+CND_ARTIFACT_NAME_Release=midterm_prob6
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/midterm_prob6
 CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
-CND_PACKAGE_NAME_Release=midtermprob2.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/midtermprob2.tar
+CND_PACKAGE_NAME_Release=midtermprob6.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/midtermprob6.tar
 #
 # include compiler specific variables
 #
